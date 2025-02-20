2465919d2d588debb02522cda026c5b286b1e596
GabrielLLR <gabriel.ribeiro@sensedia.com>
2025-02-20 11:23:12 -0300

    asdsd

--- rules/validator.go
+++ rules/validator.go
@@ -2,43 +2,61 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
-	"github.com/pb33f/libopenapi"
-	"github.com/pb33f/libopenapi/tools/resolve"
-	"gopkg.in/yaml.v2"
+	"github.com/pb33f/libopenapi/index"
+	"gopkg.in/yaml.v3"
 )
 
-// Função para resolver e salvar um arquivo OpenAPI resolvido
-func resolveAndSave(inputFile, outputFile string) error {
-	// Ler o arquivo original
-	data, err := os.ReadFile(inputFile)
+// Função para ler um arquivo local e retornar seu conteúdo como bytes
+func readFile(filePath string) ([]byte, error) {
+	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return fmt.Errorf("erro ao ler o arquivo %s: %v", inputFile, err)
+		return nil, fmt.Errorf("erro ao ler o arquivo %s: %v", filePath, err)
+	}
+	return data, nil
+}
+
+// Função para resolver as referências OpenAPI usando o rolodex
+func resolveOpenAPI(inputFile, outputFile string) error {
+	// Ler o arquivo de entrada
+	data, err := readFile(inputFile)
+	if err != nil {
+		return err
 	}
 
-	// Criar um novo documento OpenAPI
-	doc, err := libopenapi.NewDocument(data)
-	if err != nil {
-		return fmt.Errorf("erro ao processar OpenAPI: %v", err)
+	// Criar um nó YAML a partir do arquivo
+	var rootNode yaml.Node
+	if err := yaml.Unmarshal(data, &rootNode); err != nil {
+		return fmt.Errorf("erro ao fazer unmarshal do YAML: %v", err)
+	}
+
+	// Criar uma configuração para o indexador (desabilitando lookups externos)
+	indexConfig := index.CreateClosedAPIIndexConfig()
+
+	// Criar um novo rolodex para gerenciar referências
+	rolodex := index.NewRolodex(indexConfig)
+
+	// Definir o root node do rolodex
+	rolodex.SetRootNode(&rootNode)
+
+	// Indexar as referências do OpenAPI
+	if err := rolodex.IndexTheRolodex(); err != nil {
+		return fmt.Errorf("erro ao indexar as referências: %v", err)
 	}
 
 	// Resolver todas as referências
-	resolver := resolve.NewResolver(doc)
-	resolvedDoc, _, err := resolver.Resolve()
-	if err != nil {
-		return fmt.Errorf("erro ao resolver referências: %v", err)
-	}
+	rolodex.Resolve()
 
-	// Converter o OpenAPI resolvido para YAML
-	resolvedYAML, err := yaml.Marshal(resolvedDoc)
+	// Criar um YAML resolvido a partir do rolodex atualizado
+	resolvedYAML, err := yaml.Marshal(rootNode)
 	if err != nil {
 		return fmt.Errorf("erro ao converter para YAML: %v", err)
 	}
 
-	// Salvar o arquivo resolvido
-	err = os.WriteFile(outputFile, resolvedYAML, 0644)
-	if err != nil {
+	// Salvar o YAML resolvido em um novo arquivo
+	if err := ioutil.WriteFile(outputFile, resolvedYAML, 0644); err != nil {
 		return fmt.Errorf("erro ao salvar arquivo resolvido: %v", err)
 	}
 
@@ -56,12 +74,12 @@ func main() {
 	newFile := os.Args[2]
 
 	// Resolver e salvar os arquivos
-	if err := resolveAndSave(oldFile, "oldSwaggerResolve.yaml"); err != nil {
+	if err := resolveOpenAPI(oldFile, "oldSwaggerResolve.yaml"); err != nil {
 		fmt.Println("❌ Erro ao processar oldSwagger.yaml:", err)
 		os.Exit(1)
 	}
 
-	if err := resolveAndSave(newFile, "swaggerResolve.yaml"); err != nil {
+	if err := resolveOpenAPI(newFile, "swaggerResolve.yaml"); err != nil {
 		fmt.Println("❌ Erro ao processar swagger.yaml:", err)
 		os.Exit(1)
 	}
